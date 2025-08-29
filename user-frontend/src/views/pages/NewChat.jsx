bdc141240fc47bb760628e8a696416deb44edf02
shahid-mohammed-9x <shahid.mohammed@9xtechnology.com>
2025-08-29 15:36:32 +0530

    added hello message for the new-chat

--- user-frontend/src/views/pages/NewChat.jsx
+++ user-frontend/src/views/pages/NewChat.jsx
@@ -1,12 +1,17 @@
 import React, { memo } from 'react';
 import ChatFooter from '../features/chat/ChatFooter';
 import ChatLayout from '../layouts/ChatLayout';
+import { useSelector } from 'react-redux';
 
 const NewChat = () => {
+  const { profileDetails } = useSelector((state) => state.userProfileState);
   return (
     <ChatLayout>
       <div className="m-auto w-full">
         <div>
+          <p className="text-center text-4xl font-bold mb-6">
+            Welcome {profileDetails?.fullName ?? profileDetails?.email}, how can I help?
+          </p>
           <ChatFooter />
         </div>
       </div>
