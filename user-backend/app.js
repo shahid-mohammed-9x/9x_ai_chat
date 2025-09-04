5db194c6aac9c217822a1337f76021002ecd8b47
shahid-mohammed-9x <shahid.mohammed@9xtechnology.com>
2025-09-04 12:28:50 +0530

    BUG:active models in footers done

--- user-backend/app.js
+++ user-backend/app.js
@@ -23,14 +23,12 @@ if (DEVELOPMENT_MODE === "development") {
 }
 
 app.use(helmetConfig);
-app.use(ratelimitConfig);
+// app.use(ratelimitConfig);
 app.use(compression({ level: 6 }));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(corsConfig);
 app.use("/public", express.static(path.join(__dirname, "public")));
-
-
 
 // Routes
 app.use(IndexRoutes);
